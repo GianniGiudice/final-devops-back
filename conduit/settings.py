5769bec62a988a22e5a344cbff8356b7863553d9
Gianni GIUDICE <gianni.giudice@lacatholille.fr>
2021-01-18 09:46:44 +0100

    :wrench: Modification des settings dans conduit

--- conduit/settings.py
+++ conduit/settings.py
@@ -25,6 +25,11 @@ class Config(object):
         'http://localhost:4200',
         'http://0.0.0.0:4000',
         'http://localhost:4000',
+        'http://0.0.0.0:' + os.environ.get('PORT', '8080'),
+        'https://0.0.0.0:' + os.environ.get('PORT', '8080'),
+        'https://optimistic-engelbart-75b836.netlify.app',
+        'http://optimistic-engelbart-75b836.netlify.app',
+        os.environ.get('FRONT_PROD_URL', ''),
     ]
     JWT_HEADER_TYPE = 'Token'
 
